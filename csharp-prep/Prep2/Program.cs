15d4cf0958e8ff0bf18483bbbc511d9bd2a5527c
yourusername <videogameacces@yahoo.com>
2024-09-20 14:58:02 -0600

    Completed Prep 2.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Input course grade percentage: ");
+        string courseGradeInput = Console.ReadLine();
+        int courseGradeNumber = int.Parse(courseGradeInput);
+        string courseGradeOutput = "";
+        if (courseGradeNumber >= 90 )
+        {
+            courseGradeOutput = "A";
+        }
+        else if (courseGradeNumber >= 80)
+        {
+            courseGradeOutput = "B";
+        }
+        else if (courseGradeNumber >= 70)
+        {
+            courseGradeOutput = "C";
+        }
+        else if (courseGradeNumber >= 60)
+        {
+            courseGradeOutput = "D";
+        }
+        else
+        {
+            courseGradeOutput = "F";
+        }
+        
+        Console.WriteLine("");
+        Console.WriteLine($"Grade projected: {courseGradeOutput}");
+        Console.WriteLine("");
+        if (courseGradeNumber >= 70)
+        {
+            Console.WriteLine("Course: PASSED");
+        }
+        else
+        {
+            Console.WriteLine("Course: FAILED");
+        }
     }
 }
